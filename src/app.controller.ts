9eab21fbcdf8a99e1abe4bb13a92b380181d846c
Rodrigo Grigera <rodrigogrigera@gmail.com>
2025-11-08 08:55:08 -0300

    Modificacion en controlador de tracks y servicios en configuracion de modulos

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,11 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Cancion } from './app.service';
-import { get } from 'http';
 
-@Controller("canciones")
+@Controller()
 // nombramos el nombre de la ruta "canciones"
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getAll(): Cancion[] {
-    return this.appService.getCanciones();
-  }
-  @Get(":id")
-  getId(@Param('id') id: string): Cancion | undefined {
-    return this.appService.getId(Number(id));
-  }
 }
 
 
