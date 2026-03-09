e1434a3fec52a2c4b63d00777c4843df1acd085b
Rodrigo Grigera <rodrigogrigera@gmail.com>
2026-03-09 10:47:21 -0300

    agregagamos base de datos en app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TrackController } from './track/track.controller';
-import { TrackService } from './track/track.service';
-import { ArtistModule } from './artist/artist.module';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+import { TrackModule } from './track/track.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// import { TrackService } from './track/track.service';
+// import { TrackController } from './track/track.controller';
 
 @Module({
-  imports: [ArtistModule],
-  controllers: [AppController, TrackController],
-  providers: [AppService, TrackService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      
+    }) 
+    ,TrackModule] /*importamos trackModule q posee trackController y trackService.
+  por lo tanto no hay que pasar en el controller y provaiders y podemos eliminar los archivos app.controler y
+  app.service que vienen en la raiz del proyecto .   */
+
+  // controllers: [AppController,TrackController], esto que esta comentato tambien se puede eliminar xq el proyecto se maneja en modulos
+  // providers: [AppService,TrackService], esto ya existe en track module.
 })
 export class AppModule {}
