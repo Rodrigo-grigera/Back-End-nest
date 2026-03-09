6499b772e792602cc39a58aaa8e1df5b2cfa3382
Rodrigo Grigera <rodrigogrigera@gmail.com>
2026-03-09 10:46:40 -0300

    modificacion en app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+// import { Injectable } from '@nestjs/common';
 
-@Injectable()
-export class AppService {
+// @Injectable()
+// export class AppService {
 
   
-}
+// }
