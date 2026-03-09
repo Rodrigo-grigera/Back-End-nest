b2dae229e8c153981fc46e71ae2b408e7fe368b3
Rodrigo Grigera <rodrigogrigera@gmail.com>
2026-03-09 10:53:19 -0300

    Modificamos logica de negocio, interaccion con DB

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,31 +1,76 @@
-import { Injectable } from '@nestjs/common';
-import { Track } from '../track/track.interface';
-import { resolveSoa } from 'dns';
-import { matchesGlob } from 'path';
-
+import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { responseDTO } from 'src/dto/responseDTO';
+import { myExeption } from 'src/myExeption/myExeption';
+import { TrackDTO } from 'src/dto/trackDTO';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { trackEntity } from './track.entity';
 
 const BASE_URL = "http://localhost:3001/tracks"
 
 @Injectable()
 export class TrackService {
 
-async getTracks(): Promise <Track[]> {
-    const resp = await fetch(BASE_URL);
-    const allTracks = await resp.json()
-    return allTracks; 
-  }
-  
-  async getTrackId(id): Promise <Track |undefined> {
-    const res = await fetch(`${BASE_URL}/${id}`);
-    if(!res.ok){
-      console.log("Error de solicitud");
-    }else{
-      const track = await res.json();
-      return track
+  constructor(@InjectRepository(trackEntity)private readonly trackRepository : Repository <trackEntity>){}
+// con este constructor hacemos que nuestros metodos puedan interactuar con nuestra base de datos y no con un json que simule la base de datos 
+// typeorm nos provee un tipado (repository) al que le pasamos nuestra entidad y asi interactua 
+
+async getTracks(): Promise <TrackDTO | responseDTO>{
+  try{
+
+    const tracks = await this.trackRepository.find(); //find es como decir select * from tracks
+    return {
+      code: HttpStatus.OK,
+      message: "Todos los tracks",
+      data: tracks
     }
+  }catch(error){
+      return {
+        code: HttpStatus.OK,
+        message: "Todos los tracks",
+        data: error
+      }
+  }
+}
+
+
+// async getTracks(): Promise <TrackDTO[] | responseDTO> {
+//     const resp = await fetch(BASE_URL);
+//     if(!resp.ok) throw new NotFoundException("No hay tracks");
+//     else{
+//       const allTracks = await resp.json()
+//       return allTracks; 
+//     }
+//   }
+  
+  async getTrackId(id:string): Promise <responseDTO> {
+    try {
+      // const res = await fetch(`${BASE_URL}/${id}`); //get desde jspn (simulando una base de datos)
+      const res = await this.trackRepository.findOneBy({id: Number(id)})
+      
+      // if(!res.ok){
+      //   throw new myExeption("Track no existe",HttpStatus.NOT_FOUND) //las exepciones vienen predeterminadas como el resp-dto pero pasamos el msj
+      // }else{
+        // const track = await res.json();
+        return{
+          message: "track encontrado",
+          code: HttpStatus.OK,
+          data: res,
+        }
+      }
+      
+    // }
+    catch (error) {
+      console.log(error);
+    return {
+          message: "Error, track no encontrado",
+          code: HttpStatus.NOT_FOUND,
+          data: error
+        }
+      }
     }
 
-  async createTrack(track:Track): Promise <any>{
+  async createTrack(track:TrackDTO): Promise <any>{
        const res = await fetch(BASE_URL, {
         method: "POST",
         headers:{  
@@ -33,7 +78,7 @@ async getTracks(): Promise <Track[]> {
           }, 
           body: JSON.stringify(track)
        });
-       console.log(`Se Agrego track: "${track.nombre}" a la lista`)
+       return (`Se Agrego track: "${track.nombre}" a la lista`)
     }
 
     async delete(id:string): Promise<void>{
@@ -44,7 +89,7 @@ async getTracks(): Promise <Track[]> {
       return endRespon
     }
 
-    async update(id: string, body: Track): Promise<void>{
+    async update(id: string, body: TrackDTO): Promise<void>{
 
       const isTrack = await this.getTrackId(id);
       if(!isTrack){
@@ -66,7 +111,7 @@ async getTracks(): Promise <Track[]> {
     solo modificas las propiedades que se quiere, en el codigo solo cambiamos el metodo que
     pasamos en el fetch de la url "method: "PATCH" */
 
-    async updatePatch(id:string, body:Track): Promise<void>{
+    async updatePatch(id:string, body:TrackDTO): Promise<void>{
       const isTrack = await this.getTrackId(id);
       if(!isTrack){
          throw new Error("Track no encontrado");
